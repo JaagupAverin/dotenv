dbe37c530e475ce55a8ac85f811ba0242e7ea59e
Jaagup Averin <jaagup.averin@gmail.com>
2024-12-02 18:14:02 +0200

    Update.

--- setup.zsh
+++ setup.zsh
@@ -1,25 +1,30 @@
 #!/bin/zsh
 
 sudo apt-get update
-sudo apt-get upgrade
 
 # Install basic tools
-sudo apt-get install -y curl wireguard tmux fzf picocom ncdu net-tools ffmpeg
+sudo apt-get install -y curl wireguard tmux fzf picocom ncdu net-tools ffmpeg xxd build-essential linux-headers-`uname -r` dkms
 sudo apt-get install -y default-jre
 
 # Install apps
 sudo apt-get install -y telegram-desktop
 
 # Install rye
-curl -sSf https://rye.astral.sh/get | bash
+if type rye &> /dev/null; then
+    echo "Rye installed"
+else
+    curl -sSf https://rye.astral.sh/get | bash
+fi
 
 # Apply user conf
 ./apply.zsh
 
 # Setup tmux
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-tmux source ~/.tmux.conf
-# And then enter (prefix + I) inside tmux to install plugins!
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    tmux source ~/.tmux.conf
+    # And then enter (prefix + I) inside tmux to install plugins!
+fi
 
 # Configure KDE manually
-# Install apps manually: CLion, VirtualBox, Zed, etc
+# Install apps manually: CLion, Slack, VirtualBox, Zed, etc
